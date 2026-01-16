652e1b44296ca89034f403dacc20364536d63d05
AtheeshMogaveer <atheesh145@gmail.com>
2026-01-16 18:29:45 +0530

    Day5Node second

--- Nodejs/Day5/script.js
+++ Nodejs/Day5/script.js
@@ -4,5 +4,10 @@ http
     res.end("Hi Bro thandani thano");
   })
   .listen(4500);
-
+http
+  .createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.end("<b>Hi Bro thandani thano</b>");
+  })
+  .listen(4800);
 console.log("Helloo");
