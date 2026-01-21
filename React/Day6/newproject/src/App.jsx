05208f5021a6bf4f111cd45fc0f742a159cb3781
AtheeshMogaveer <atheesh145@gmail.com>
2026-01-21 15:07:23 +0530

    Day6React first

---
+++ React/Day6/newproject/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Button from "./components/Button";
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Day6/newproject/src/components/Button.jsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <button className="bg-red-600 w-[100px]">Hello</button>;
+}
+
+export default Button;
