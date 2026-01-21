8998b22c97036957e2dfd46cb2505c2fb3ce482f
AtheeshMogaveer <atheesh145@gmail.com>
2026-01-21 14:42:43 +0530

    ReactDay5 first

---
+++ React/Day5/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./components/button/Button";
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Day5/vite-project/src/components/button/Button.jsx
@@ -0,0 +1,6 @@
+import styles from "./Button.module.css";
+function Button() {
+  return <button className={styles.btn}>This is</button>;
+}
+
+export default Button;
